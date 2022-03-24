c676faee84dd7ab08e11fc48b78f7bcdb37daa6f
Akirakone <ca646@khels.org>
2022-03-23 20:20:08 -0400

    pokeapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
-import { useState } from 'react';
-
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+     
+      useEffect(()=>{
+          axios.get("https://pokeapi.co/api/v2/pokemon?limit=898")
+              .then(res=>{setPokemon(res.data.results);})
+              .catch(err=>console.log(err))
+      }, [])
+      
 
-  const onClickHandler = () => {
-    fetch ("https://pokeapi.co/api/v2/pokemon?limit=898")
-    .then(res => res.json())
-    .then(res => {
-      console.log(res);
-      setPokemon(res.results);
-    })
-    .catch(err => console.log(err)) 
-  }
+      const onClickHandler =(event)=>{
+        event.preventDefault();
+      }
 
-
-  return (
-    <div className="App">
+      return(
+              <div className="App">
       <h1>Poke-Fetch</h1>
       <button onClick={onClickHandler} >Fetch Pokemon</button>
       <ul>
@@ -28,7 +28,38 @@ function App() {
         }
       </ul>
     </div>
-  );
-}
+      )
+  }
+
+
+//without using axios Code HERE
+// function App() {
+//   const [pokemon, setPokemon] = useState([]);
+
+//   const onClickHandler = () => {
+//     fetch ("https://pokeapi.co/api/v2/pokemon?limit=898")
+//     .then(res => res.json())
+//     .then(res => {
+//       console.log(res);
+//       setPokemon(res.results);
+//     })
+//     .catch(err => console.log(err)) 
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <h1>Poke-Fetch</h1>
+//       <button onClick={onClickHandler} >Fetch Pokemon</button>
+//       <ul>
+//         {
+//           pokemon.map((pokemon, i ) => {
+//             return <li key= {i}>{pokemon.name}</li>
+//           })
+//         }
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
